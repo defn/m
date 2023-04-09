85d6c665157672a8442f0deb0255d0297a1c2ab3
amanibhavam <iam@defn.sh>
2023-04-09 08:53:10 +0000

    move to infra/defn

--- app/infra/defn/flake.nix
+++
@@ -1,8 +0,0 @@
-{
-  inputs.infra.url = github:defn/m/app-cmd-infra-0.0.25?dir=app/cmd/infra;
-  outputs = inputs: inputs.infra.inputs.app.cdktfMain rec {
-    src = ./.;
-    infra = inputs.infra;
-    infra_cli = "infra";
-  };
-}
