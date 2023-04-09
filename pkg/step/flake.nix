0a4192453bf614a7a4e95fdb7fce1d7a76922fb3
amanibhavam <iam@defn.sh>
2023-04-09 06:43:56 +0000

    add step/

---
+++ pkg/step/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs.pkg.url = github:defn/m/pkg-pkg-0.0.3?dir=pkg/pkg;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+
+    url_template = input: "https://github.com/smallstep/cli/releases/download/v${input.vendor}/step_${input.os}_${input.vendor}_${input.arch}.tar.gz";
+
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 */bin/step $out/bin/step
+    '';
+
+    downloads = {
+      
+
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-ch1528RjTp5AjiQy0HIZ5nhIkGtAlE3U7pl5u4h4Gug="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-ilhnpRXOD+TJgIUciJgGDqOECEFQVDhMYiDK8uJc2H8="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-+1btQfhauQiBgJxzdLK1NxppLjNtMFgYACpAVbBa2pg="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-W9TRLou5tDjiCKyW/0E7Hs0zKmwepUbT8yom9xQXkvA="; # aarch64-darwin
+      };
+    };
+  };
+}
