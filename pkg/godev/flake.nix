28a6f4e3b25807fd9dddcd0f646d5f8abeaa367a
amanibhavam <iam@defn.sh>
2023-04-09 06:41:08 +0000

    add godev/

---
+++ pkg/godev/flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    goreleaser.url = github:defn/m/?dir=pkg/goreleaser;
+    buf.url = github:defn/m/pkg-buf-1.17.0-3?dir=pkg/buf;
+    operatorsdk.url = github:defn/m/?dir=pkg/operatorsdk;
+    cosign.url = github:defn/m/pkg-cosign-2.0.1-3?dir=pkg/cosign;
+    latest.url = github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db; # nixos-unstable https://lazamar.co.uk/nix-versions/
+  };
+
+  outputs = inputs: inputs.goreleaser.inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs =
+        with (import inputs.latest { system = ctx.system; }); [
+          gcc
+          go_1_20
+          bashInteractive
+          gotools
+          go-tools
+          golangci-lint
+          go-outline
+          gopkgs
+          delve
+          gopls
+          inputs.buf.defaultPackage.${ctx.system}
+          inputs.operatorsdk.defaultPackage.${ctx.system}
+          inputs.goreleaser.defaultPackage.${ctx.system}
+          inputs.cosign.defaultPackage.${ctx.system}
+        ];
+    };
+  };
+}
