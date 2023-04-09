226e485ccfc7deb69fdcdda52a09033dc6188fa5
amanibhavam <iam@defn.sh>
2023-04-09 06:42:46 +0000

    add n/

---
+++ pkg/n/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    pkg.url = github:defn/m/pkg-pkg-0.0.3?dir=pkg/pkg;
+  };
+
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.bashBuilder {
+      inherit src;
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -a $src/bin/n $src/bin/n-* $out/bin/
+        chmod 755 $out/bin/*
+      '';
+    };
+  };
+}
