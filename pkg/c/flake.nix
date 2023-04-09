e1188a88c5320d4163119c1b9076f810d829e751
amanibhavam <iam@defn.sh>
2023-04-09 06:39:12 +0000

    add c/

---
+++ pkg/c/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs.cue.url = "github:defn/m/pkg-cue-0.5.0-beta.5-4?dir=pkg/cue";
+  inputs.hof.url = "github:defn/m/pkg-hof-0.6.8-beta.12-4?dir=pkg/hof";
+  outputs = inputs: inputs.cue.inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.bashBuilder {
+      src = ./bin;
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -a $src/* $out/bin/
+        chmod 755 $out/bin/*
+      '';
+
+      propagatedBuildInputs = [
+        inputs.cue.defaultPackage.${ctx.system}
+        inputs.hof.defaultPackage.${ctx.system}
+      ];
+    };
+  };
+}
