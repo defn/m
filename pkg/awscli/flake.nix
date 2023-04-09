a30f2deff0dcf1fc155f5ce5cc477f07f2fe13d2
amanibhavam <iam@defn.sh>
2023-04-09 06:38:27 +0000

    add awscli/

---
+++ pkg/awscli/flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs.pkg.url = github:defn/m/pkg-pkg-0.0.3?dir=pkg/pkg;
+  outputs = inputs: inputs.pkg.downloadMain
+    rec {
+      src = ./.;
+
+      # https://raw.githubusercontent.com/aws/aws-cli/v2/CHANGELOG.rst
+      url_template = input:
+        if input.os == "linux" then
+          "https://awscli.amazonaws.com/awscli-exe-${input.os}-${input.arch}-${input.vendor}.zip"
+        else
+          "https://awscli.amazonaws.com/AWSCLIV2-${input.vendor}.pkg";
+
+      installPhase = pkg: ''
+        case $src in
+          *.zip)
+            unzip $src
+            mkdir -p $out/bin $out/awscli
+            aws/install -i $out/awscli -b $out/bin
+            ;;
+          *.pkg)
+              xar -xf $src
+              mkdir -p $out/bin $out/awscli
+              cd aws-cli.pkg
+              zcat Payload | (cd $out && cpio -i && mkdir awscli/v2 && mv aws-cli awscli/v2/dist && ln -nfs ../awscli/v2/dist/aws ../awscli/v2/dist/aws_completer bin/)
+            ;;
+        esac
+      '';
+
+      downloads = {
+        options = pkg: {
+          dontUnpack = true;
+          buildInputs = with pkg.ctx.pkgs; [
+            unzip
+            xar
+            cpio
+          ];
+        };
+
+        "x86_64-linux" = rec {
+          os = "linux";
+          arch = "x86_64";
+          sha256 = "sha256-1vw1f+KrZVyCoifF7QkmhvOtOiiDjF6hctqwmLxtgQ8="; # x86_64-linux
+        };
+        "aarch64-linux" = rec {
+          os = "linux";
+          arch = "aarch64";
+          sha256 = "sha256-bKJullQODftVe6xla23XFmeK/S4UHtR2WUxTDDfsBpk="; # aarch64-linux
+        };
+        "x86_64-darwin" = rec {
+          os = "darwin";
+          sha256 = "sha256-CB9hDTvkPWBbHLLiejoo0rV2CwaZnRdnkybvR94tECA="; # x86_64-darwin
+        };
+        "aarch64-darwin" = rec {
+          os = "darwin";
+          sha256 = "sha256-CB9hDTvkPWBbHLLiejoo0rV2CwaZnRdnkybvR94tECA="; # aarch64-darwin
+        };
+      };
+    };
+}
