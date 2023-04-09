5d02b2503646e4ab61d12d328223b477151c1a80
amanibhavam <iam@defn.sh>
2023-04-09 19:54:55 +0000

    no tkn, use buildkite

--- pkg/kubernetes/flake.nix
+++ pkg/kubernetes/flake.nix
@@ -6,9 +6,8 @@
     helm.url = github:defn/m/pkg-helm-3.11.2-3?dir=pkg/helm;
     kustomize.url = github:defn/m/pkg-kustomize-5.0.1-3?dir=pkg/kustomize;
     stern.url = github:defn/m/pkg-stern-1.24.0-3?dir=pkg/stern;
-    argo.url = github:defn/m/pkg-argo-3.4.6-4?dir=pkg/argo;
+    argoworkflows.url = github:defn/m/pkg-argoworkflows-3.4.6-5?dir=pkg/argoworkflows;
     argocd.url = github:defn/m/pkg-argocd-2.6.7-4?dir=pkg/argocd;
-    tkn.url = github:defn/m/pkg-tkn-0.30.0-4?dir=pkg/tkn;
     kn.url = github:defn/m/pkg-kn-1.9.2-3?dir=pkg/kn;
     vcluster.url = github:defn/m/pkg-vcluster-0.14.2-3?dir=pkg/vcluster;
     kubevirt.url = github:defn/m/pkg-kubevirt-0.59.0-3?dir=pkg/kubevirt;
