6bb080f5ffe152c47ac6a6622ba850311bf17c78
amanibhavam <iam@defn.sh>
2023-04-09 06:44:07 +0000

    add tailscale/

---
+++ pkg/tailscale/flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs.pkg.url = github:defn/m/pkg-pkg-0.0.3?dir=pkg/pkg;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+
+    etc_src = ./etc;
+
+    extend = pkg: {
+      apps.default = {
+        type = "app";
+        program = "${pkg.this.defaultPackage}/bin/tailscale";
+      };
+
+      apps.tailscaled = {
+        type = "app";
+        program = "${pkg.this.defaultPackage}/bin/tailscaled";
+      };
+    };
+
+    url_template = input: "https://pkgs.tailscale.com/stable/tailscale_${input.vendor}_${input.arch}.tgz";
+
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin
+
+      case "${pkg.config.os}" in
+        darwin)
+          install -m 0755 ${etc_src}/tailscale-darwin $out/bin/tailscale
+          ;;
+        *)
+          install -m 0755 */tailscale $out/bin/tailscale
+          install -m 0755 */tailscaled $out/bin/tailscaled
+          ;;
+      esac
+    '';
+
+    downloads = {
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-kd7IQfaLMWDRwvF61p0C1XqR8OatDOSma2NT/+HBUfI="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-H8bdGnJ9rVSZoObPRsygy8WaVklsSELeI2UAZCV73Iw="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-kd7IQfaLMWDRwvF61p0C1XqR8OatDOSma2NT/+HBUfI="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-H8bdGnJ9rVSZoObPRsygy8WaVklsSELeI2UAZCV73Iw="; # aarch64-darwin
+      };
+    };
+  };
+}
