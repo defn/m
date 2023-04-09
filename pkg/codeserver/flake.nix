fffbd513515b3215447401859c3d82cfbb961fea
amanibhavam <iam@defn.sh>
2023-04-09 06:39:50 +0000

    add codeserver/

---
+++ pkg/codeserver/flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs.pkg.url = github:defn/m/pkg-pkg-0.0.3?dir=pkg/pkg;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+
+    extend = pkg: {
+      apps.default = {
+        type = "app";
+        program = "${pkg.this.defaultPackage}/bin/code-server";
+      };
+    };
+
+    url_template = input: "https://github.com/coder/code-server/releases/download/v${input.vendor}/code-server-${input.vendor}-${input.os}-${input.arch}.tar.gz";
+
+    installPhase = pkg:
+      ''
+        install -m 0755 -d $out $out/bin $out/lib
+        rsync -ia . $out/lib/.
+        mv -f $out/lib/code-server-${pkg.config.vendor}-*  $out/lib/code-server-${pkg.config.vendor}
+        ln -fs $out/lib/code-server-${pkg.config.vendor}/bin/code-server $out/bin/code-server
+        ln -fs $out/lib/code-server-${pkg.config.vendor}/lib/vscode/bin/helpers/browser.sh $out/bin/browser.sh
+      '';
+
+    downloads = {
+      options = pkg: {
+        dontFixup = true;
+        buildInputs = with pkg.ctx.pkgs; [ rsync ];
+      };
+
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-TrIzBUlB7CmMrsb8hN+6CnLBvF+twP5IlrEPP0opHVE="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-gb9QrA9KSbj5LjqyeBaksl2V3P9o64GzEAkOFDYM+cQ="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "macos";
+        arch = "amd64";
+        sha256 = "sha256-ovIvk7WB5fsvULymiDV/C31ZGEsy5gQQ5gRCCs9ZTI8="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "macos";
+        arch = "amd64"; # code-server not avaialble for darwin arm64
+        sha256 = "sha256-ovIvk7WB5fsvULymiDV/C31ZGEsy5gQQ5gRCCs9ZTI8="; # aarch64-darwin
+      };
+    };
+  };
+}
