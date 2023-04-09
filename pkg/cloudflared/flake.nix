b8c2e445f17162cbf8c1323eef8afcbf3b8b6cb5
amanibhavam <iam@defn.sh>
2023-04-09 06:39:26 +0000

    add cloudflared/

---
+++ pkg/cloudflared/flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs.pkg.url = github:defn/m/pkg-pkg-0.0.3?dir=pkg/pkg;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+
+    url_template = input:
+      if input.os == "linux" then
+        "https://github.com/cloudflare/cloudflared/releases/download/${input.vendor}/cloudflared-${input.os}-${input.arch}"
+      else
+        "https://github.com/cloudflare/cloudflared/releases/download/${input.vendor}/cloudflared-${input.os}-${input.arch}.tgz";
+
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin
+      case "$src" in
+        *.tgz)
+          tar xvfz $src
+          install -m 0755 cloudflared $out/bin/cloudflared
+          ;;
+        *)
+          install -m 0755 $src $out/bin/cloudflared
+          ;;
+      esac
+    '';
+
+    downloads = {
+      options = pkg: { dontUnpack = true; dontFixup = true; };
+
+      "x86_64-linux" = rec {
+        os = "linux";
+        arch = "amd64";
+        sha256 = "sha256-cqdQ16BDsq4pFHBxD6+oFqsQSmASDsZyHXwfu/JMhVg="; # x86_64-linux
+      };
+      "aarch64-linux" = rec {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-GYnttSRPy7hCCpRlWyGT+cEDdYQY592up7emOFKZMTU="; # aarch64-linux
+      };
+      "x86_64-darwin" = rec {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-eMVCuytrXJM0RhLZjgl1gjKifPRzaopWfxqxWdUbXBA="; # x86_64-darwin
+      };
+      "aarch64-darwin" = rec {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-eMVCuytrXJM0RhLZjgl1gjKifPRzaopWfxqxWdUbXBA="; # aarch64-darwin
+      };
+    };
+  };
+}
