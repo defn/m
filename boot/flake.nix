4f045244b7c327eea2c450a6addd9102cd2fd985
amanibhavam <iam@defn.sh>
2023-04-11 05:59:08 +0000

    import a pared down boot

---
+++ boot/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    godev.url = github:defn/m/pkg-godev-0.0.67?dir=pkg/godev;
+  };
+
+  outputs = inputs:
+    inputs.godev.inputs.goreleaser.inputs.pkg.main rec {
+      src = ./.;
+      defaultPackage = ctx: ctx.wrap.nullBuilder {
+        propagatedBuildInputs = with ctx.pkgs; [
+          bashInteractive
+          rsync
+          inputs.godev.defaultPackage.${ctx.system}
+        ];
+      };
+
+      packages = ctx: rec {
+        devShell = ctx: ctx.wrap.devShell {
+          devInputs = [
+            (defaultPackage ctx)
+          ];
+        };
+      };
+    };
+}
