fef9a7327432e52927d256535ce5c186af6aaf17
amanibhavam <iam@defn.sh>
2023-04-15 19:12:24 +0000

    upgrade

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
     gomod2nix.url = github:defn/gomod2nix/1.5.0-9;
     godev.url = github:defn/m/pkg-godev-0.0.69?dir=pkg/godev;
     nodedev.url = github:defn/m/pkg-nodedev-0.0.43?dir=pkg/nodedev;
+    localdev.url = github:defn/m/pkg-localdev-0.0.100?dir=pkg/localdev;
+    development.url = github:defn/m/pkg-development-0.0.44?dir=pkg/development;
     latest.url = github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db;
   };
 
@@ -21,9 +23,11 @@
         ctx.wrap.nullBuilder {
           propagatedBuildInputs = with ctx.pkgs; [
             goPkgs.gomod2nix
-            bashInteractive
             inputs.godev.defaultPackage.${ctx.system}
             inputs.nodedev.defaultPackage.${ctx.system}
+            inputs.localdev.defaultPackage.${ctx.system}
+            inputs.development.defaultPackage.${ctx.system}
+            bashInteractive
           ];
         };
 
