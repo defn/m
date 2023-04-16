71a5f4160c3a79b94567a19b996f00edce281dc1
amanibhavam <iam@defn.sh>
2023-04-15 17:32:51 -0700

    fix locks

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
     gomod2nix.url = github:defn/gomod2nix/1.5.0-9;
-    godev.url = github:defn/m/pkg-godev-0.0.69?dir=pkg/godev;
+    godev.url = github:defn/m/pkg-godev-0.0.70?dir=pkg/godev;
     nodedev.url = github:defn/m/pkg-nodedev-0.0.43?dir=pkg/nodedev;
-    localdev.url = github:defn/m/pkg-localdev-0.0.100?dir=pkg/localdev;
-    development.url = github:defn/m/pkg-development-0.0.45?dir=pkg/development;
+    localdev.url = github:defn/m/pkg-localdev-0.0.101?dir=pkg/localdev;
+    development.url = github:defn/m/pkg-development-0.0.46?dir=pkg/development;
     latest.url = github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db;
   };
 
