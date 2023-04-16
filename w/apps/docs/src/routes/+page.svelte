b2d650f00c09f3b28042a0c12032486527daa75d
amanibhavam <iam@defn.sh>
2023-04-16 22:14:30 +0000

    add some text

--- w/apps/docs/src/routes/+page.svelte
+++ w/apps/docs/src/routes/+page.svelte
@@ -11,6 +11,11 @@
   <div>
     <h1>Docs</h1>
 
-    <Counter/>
+    <Counter />
+
+    <p>
+      Lorem ipsum dolor sit amet. Non dicta ipsaId iure At eius quae. Eum quidem voluptatem Ut fugit
+      ut temporibus eaque eos molestias dolorem id facere voluptatem?
+    </p>
   </div>
 </section>
--- w/packages/ui/src/routes/+page.svelte
+++ w/packages/ui/src/routes/+page.svelte
@@ -10,6 +10,12 @@
 <section>
   <div>
     <h1>UI</h1>
+
     <Counter />
+
+    <p>
+      Lorem ipsum dolor sit amet. Non dicta ipsaId iure At eius quae. Eum quidem voluptatem Ut fugit
+      ut temporibus eaque eos molestias dolorem id facere voluptatem?
+    </p>
   </div>
 </section>
