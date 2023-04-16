7ffbb0948d19470c8fbfc7c35df7f8cd58d795c7
amanibhavam <iam@defn.sh>
2023-04-15 19:57:53 -0700

    fix digest

--- pkg/gum/flake.nix
+++ pkg/gum/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.pkg.url = github:defn/m/pkg-pkg-0.0.3?dir=pkg/pkg;
+  inputs.pkg.url = github:defn/m/pkg-pkg-0.0.5?dir=pkg/pkg;
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
