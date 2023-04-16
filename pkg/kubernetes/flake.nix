30b8026bc414593f10b68607696dd19db5a2938b
amanibhavam <iam@defn.sh>
2023-04-15 20:01:28 -0700

    fix digest

--- pkg/kubernetes/flake.nix
+++ pkg/kubernetes/flake.nix
@@ -1,14 +1,14 @@
 {
   inputs = {
-    kubectl.url = github:defn/m/pkg-kubectl-1.25.8-3?dir=pkg/kubectl;
-    k3d.url = github:defn/m/pkg-k3d-5.4.9-3?dir=pkg/k3d;
-    k9s.url = github:defn/m/pkg-k9s-0.27.3-3?dir=pkg/k9s;
-    helm.url = github:defn/m/pkg-helm-3.11.3-1?dir=pkg/helm;
+    kubectl.url = github:defn/m/pkg-kubectl-1.25.8-5?dir=pkg/kubectl;
+    k3d.url = github:defn/m/pkg-k3d-5.4.9-5?dir=pkg/k3d;
+    k9s.url = github:defn/m/pkg-k9s-0.27.3-5?dir=pkg/k9s;
+    helm.url = github:defn/m/pkg-helm-3.11.3-3?dir=pkg/helm;
     kustomize.url = github:defn/m/pkg-kustomize-5.0.1-3?dir=pkg/kustomize;
     stern.url = github:defn/m/pkg-stern-1.25.0-1?dir=pkg/stern;
-    argoworkflows.url = github:defn/m/pkg-argoworkflows-3.4.7-1?dir=pkg/argoworkflows;
-    argocd.url = github:defn/m/pkg-argocd-2.6.7-5?dir=pkg/argocd;
-    kn.url = github:defn/m/pkg-kn-1.9.2-3?dir=pkg/kn;
+    argoworkflows.url = github:defn/m/pkg-argoworkflows-3.4.7-3?dir=pkg/argoworkflows;
+    argocd.url = github:defn/m/pkg-argocd-2.6.7-6?dir=pkg/argocd;
+    kn.url = github:defn/m/pkg-kn-1.9.2-5?dir=pkg/kn;
     vcluster.url = github:defn/m/pkg-vcluster-0.15.0-1?dir=pkg/vcluster;
     kubevirt.url = github:defn/m/pkg-kubevirt-0.59.0-3?dir=pkg/kubevirt;
   };
