d8177ecfd04e941ff02e1689cc7853f2639eccf7
amanibhavam <iam@defn.sh>
2023-04-16 22:01:59 +0000

    remove all styles

--- w/packages/ui/components/Header.svelte
+++ w/packages/ui/components/Header.svelte
@@ -1,43 +1,51 @@
 <script>
-  import "../src/app.postcss";
-  import {
-    Navbar,
-    NavBrand,
-    NavLi,
-    NavUl,
-    NavHamburger,
-    Button,
-  } from "flowbite-svelte";
+  import '../src/app.postcss';
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
 </script>
 
-<header>
-  <Navbar let:hidden let:toggle>
-    <NavBrand href="/">
-      <img
-        src="https://flowbite.com/docs/images/logo.svg"
-        class="mr-3 h-6 sm:h-9"
-        alt="logo"
-      />
-      <span
-        class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-      >
-        DEFN
-      </span>
-    </NavBrand>
-    <div class="flex md:order-2">
-      <Button size="sm">Get started</Button>
-      <NavHamburger on:click={toggle} />
-    </div>
-    <NavUl {hidden} class="order-1">
-      <NavLi href="/">Home</NavLi>
-      <NavLi href="/about">About</NavLi>
-    </NavUl>
-  </Navbar>
-</header>
+<div class="app">
+  <header>
+    <Navbar let:hidden let:toggle>
+      <NavBrand href="/">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="logo" />
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          DEFN
+        </span>
+      </NavBrand>
+      <div class="flex md:order-2">
+        <Button size="sm">Get started</Button>
+        <NavHamburger on:click={toggle} />
+      </div>
+      <NavUl {hidden} class="order-1">
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/about">About</NavLi>
+      </NavUl>
+    </Navbar>
+  </header>
+
+  <main>
+    <slot />
+  </main>
+</div>
 
 <style>
   header {
     display: flex;
     justify-content: space-between;
+  }
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 64rem;
+    margin: 0 auto;
+    box-sizing: border-box;
   }
 </style>
