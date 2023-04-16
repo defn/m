5eb153f9d067ccebae7c9d343e6a681fa187e401
amanibhavam <iam@defn.sh>
2023-04-15 20:19:19 -0700

    fixing digests

--- pkg/cloud/flake.nix
+++ pkg/cloud/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    terraform.url = github:defn/m/pkg-terraform-1.4.5-1?dir=pkg/terraform;
-    packer.url = github:defn/m/pkg-packer-1.8.6-3?dir=pkg/packer;
-    step.url = github:defn/m/pkg-step-0.24.3-1?dir=pkg/step;
+    terraform.url = github:defn/m/pkg-terraform-1.4.5-3?dir=pkg/terraform;
+    packer.url = github:defn/m/pkg-packer-1.8.6-5?dir=pkg/packer;
+    step.url = github:defn/m/pkg-step-0.24.3-3?dir=pkg/step;
     awscli.url = github:defn/m/pkg-awscli-2.11.13-3?dir=pkg/awscli;
   };
 
