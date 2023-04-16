7f3f867d75a4285d078e7c2a385157b143a38079
amanibhavam <iam@defn.sh>
2023-04-16 05:19:12 +0000

    adding bare bones defn

---
+++ web/apps/defn/src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+  import Counter from "./Counter.svelte";
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <div>
+    <h1>33 ipsam labore eos fugiat asperiores.</h1>
+
+    <Counter />
+
+    <p>
+      Lorem ipsum dolor sit amet. Non dicta ipsaId iure At eius quae. Eum quidem
+      voluptatem <em>Ut fugit ut temporibus eaque eos molestias dolorem</em> id facere
+      voluptatem?
+    </p>
+
+    <ol>
+      <li>At sunt rerum sit laborum omnis qui accusantium deserunt.</li>
+      <li>Aut delectus quos qui saepe optio.</li>
+    </ol>
+
+    <p>Quo animi <code>exercitationem</code> et illo unde nam tenetur dolore.</p>
+
+    <h2>Aut ullam beatae a tenetur quae.</h2>
+
+    <p>
+      33 quidem deleniti <a href="https://www.loremipzum.com" target="_blank"
+        >33 neque</a
+      >
+      ut officia beatae eum sapiente eaque. Et eveniet illum vel quam explicaboEt
+      rerum ut amet nobis sed quisquam expedita
+      <strong>sed assumenda beatae</strong>
+      sit magnam dicta. Sit blanditiis molestiae <em>Ut expedita</em> id nisi reiciendis.
+    </p>
+
+    <ul>
+      <li>Et laborum ipsum et mollitia beatae est consequatur internos.</li>
+      <li>Ad ipsum nihil et officiis ullam.</li>
+      <li>Ut quia numquam est explicabo modi hic error dolorem!</li>
+      <li>Aut suscipit accusamus et eligendi facere.</li>
+      <li>
+        Ut nihil fugiat id omnis distinctio qui eaque enim qui soluta sequi.
+      </li>
+    </ul>
+
+    <h3>Ea quia consectetur 33 vero accusamus non sunt perspiciatis!</h3>
+
+    <p>
+      Est quibusdam rerum <em
+        >Id maxime ea voluptatum obcaecati qui architecto autem</em
+      > aut ratione adipisci qui facilis inventore At dolorum dolor. 33 earum reiciendis
+      qui necessitatibus harumnam natus ea exercitationem harum!
+    </p>
+  </div>
+</section>
