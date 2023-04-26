9f4fa57dec2bf2877b96d38727766a5edf071d4c
amanibhavam <iam@defn.sh>
2023-04-25 18:49:35 -0700

    remove operatorsdk

--- pkg/godev/flake.nix
+++ pkg/godev/flake.nix
@@ -1,9 +1,8 @@
 {
   inputs = {
-    goreleaser.url = github:defn/m/pkg-goreleaser-1.17.2-1?dir=pkg/goreleaser;
     buf.url = github:defn/m/pkg-buf-1.17.0-6?dir=pkg/buf;
-    operatorsdk.url = github:defn/m/pkg-operatorsdk-1.28.0-7?dir=pkg/operatorsdk;
     cosign.url = github:defn/m/pkg-cosign-2.0.2-1?dir=pkg/cosign;
+    goreleaser.url = github:defn/m/pkg-goreleaser-1.17.2-1?dir=pkg/goreleaser;
     latest.url = github:NixOS/nixpkgs?rev=e3b18e82da9ab82b572d70c162d4e13a058aeb7d;
   };
 
@@ -23,9 +22,8 @@
           gopls
           go_1_20
           inputs.buf.defaultPackage.${ctx.system}
-          inputs.operatorsdk.defaultPackage.${ctx.system}
-          inputs.goreleaser.defaultPackage.${ctx.system}
           inputs.cosign.defaultPackage.${ctx.system}
+          inputs.goreleaser.defaultPackage.${ctx.system}
           bashInteractive
         ];
     };
